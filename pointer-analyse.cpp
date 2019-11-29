dd69dd1d13bb8aa39e6415eeccf2f97180db09aa
ycdxsb <1452740432@qq.com>
2019-11-29 06:05:09 -0800

    init

---
+++ pointer-analyse.cpp
@@ -0,0 +1,120 @@
+//===- Hello.cpp - Example code from "Writing an LLVM Pass" ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements two versions of the LLVM "Hello World" pass described
+// in docs/WritingAnLLVMPass.html
+//
+//===----------------------------------------------------------------------===//
+
+#include <llvm/Support/CommandLine.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Support/ToolOutputFile.h>
+
+#if LLVM_VERSION_MAJOR >= 4
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
+
+#else
+#include <llvm/Bitcode/ReaderWriter.h>
+#endif
+
+#include <llvm/Transforms/Scalar.h>
+
+#include "Liveness.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+#if LLVM_VERSION_MAJOR >= 4
+static ManagedStatic<LLVMContext> GlobalContext;
+static LLVMContext &getGlobalContext() { return *GlobalContext; }
+#endif
+
+/* In LLVM 5.0, when  -O0 passed to clang , the functions generated with clang will
+* have optnone attribute which would lead to some transform passes disabled, like mem2reg.
+*/
+#if LLVM_VERSION_MAJOR == 5
+struct EnableFunctionOptPass : public FunctionPass {
+    static char ID;
+    EnableFunctionOptPass() :FunctionPass(ID) {}
+    bool runOnFunction(Function & F) override {
+        if (F.hasFnAttribute(Attribute::OptimizeNone))
+        {
+            F.removeFnAttr(Attribute::OptimizeNone);
+        }
+        return true;
+    }
+};
+
+char EnableFunctionOptPass::ID = 0;
+#endif
+
+///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 3
+struct FuncPtrPass : public ModulePass {
+    static char ID; // Pass identification, replacement for typeid
+    FuncPtrPass() : ModulePass(ID) {}
+
+
+    bool runOnModule(Module &M) override {
+        errs() << "Hello: ";
+        errs().write_escaped(M.getName()) << '\n';
+//        M.dump();
+        M.print(llvm::errs(), nullptr);
+        errs() << "------------------------------\n";
+        return false;
+    }
+};
+
+
+char FuncPtrPass::ID = 0;
+static RegisterPass<FuncPtrPass> X("funcptrpass", "Print function call instruction");
+
+char Liveness::ID = 0;
+static RegisterPass<Liveness> Y("liveness", "Liveness Dataflow Analysis");
+
+static cl::opt<std::string>
+InputFilename(cl::Positional,
+              cl::desc("<filename>.bc"),
+              cl::init(""));
+
+
+int main(int argc, char **argv) {
+   LLVMContext &Context = getGlobalContext();
+   SMDiagnostic Err;
+   // Parse the command line to read the Inputfilename
+   cl::ParseCommandLineOptions(argc, argv,
+                              "FuncPtrPass \n My first LLVM too which does not do much.\n");
+
+
+   // Load the input module
+   std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
+   if (!M) {
+      Err.print(argv[0], errs());
+      return 1;
+   }
+
+   llvm::legacy::PassManager Passes;
+#if LLVM_VERSION_MAJOR == 5
+   Passes.add(new EnableFunctionOptPass());
+#endif
+   ///Transform it to SSA
+   Passes.add(llvm::createPromoteMemoryToRegisterPass());
+
+   /// Your pass to print Function and Call Instructions
+   //Passes.add(new Liveness());
+   Passes.add(new FuncPtrPass());
+   Passes.run(*M.get());
+#ifndef NDEBUG
+   system("pause");
+#endif
+}
+
